eaacd60243fc9a50de78a20cd4bdcaaa6577a2e3
keshav <38889066+keshavten@users.noreply.github.com>
2018-05-08 00:07:50 +0530

    Update views.py
    issue resolved

--- jira/views.py
+++ jira/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
+import json
 
 def saveToFile(request):
 	print('RECEIVED REQUEST: ' + request.method)
 	""" logic to save the keywords to new file, which in turn will be picked by JIRA runner to get proper data"""
 	if request.method == 'POST':
-		print('RECEIVED REQUEST: ' + request.body())
-		message = 'Test data'
-		text_file = open("C://python//SearchJira.txt", "a")
-		text_file.write("- - %s" % message +"\n")
-		text_file.close()
+		received_json_data = json.loads(request.body.decode('utf-8'))
+		if 'text' in received_json_data :
+			print('***********Received Text********* '+ received_json_data['text'])
+			message = received_json_data['text']
+			text_file = open("C://python//SearchJira.txt", "a")
+			text_file.write("- - %s" % message +"\n")
+			text_file.close()
 		print('Chatbot : ', 'Thank you for the inputs. We will initiate the serach. Please search again after few minutes.')
 		return JsonResponse({'text':'Thank you for the inputs. We will initiate the serach. Please search again after few minutes.'}) 
 	else: #GET
