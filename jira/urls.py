20764f1b366f22804d1fdfe8dc047f938f7cc186
keshav <38889066+keshavten@users.noreply.github.com>
2018-05-09 01:10:54 +0530

    Update urls.py

--- jira/urls.py
+++ jira/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url('', views.saveToFile , name='saveToFile'),
+    url(r'^saveToFile', views.saveToFile , name='saveToFile'),
+	url(r'^trainBot', views.trainBot , name='trainBot'),
+	url(r'^runJiraUtil', views.runJiraUtil , name='runJiraUtil'),
 ]
